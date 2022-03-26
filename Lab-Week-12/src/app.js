114dec984908020c76b8f9502e12f7051ae07d85
AndrewTake <andrew6041@gmail.com>
2022-03-25 18:56:42 -0700

    created a function that takes a single  element and logs to console

--- Lab-Week-12/src/app.js
+++ Lab-Week-12/src/app.js
@@ -1,8 +1,45 @@
-function selectElement() { 
-    
-    let select = document.querySelector(".one")
-    console.log(select)
-    
-    }
-selectElement()
 
+    
+// const select = document.querySelector("#side-nav");
+// console.log(select);
+    
+function selectSideNav(){
+    const selector = document.querySelector("#side-nav");
+    console.log(selector);
+}
+
+// function selectStudents() {
+// const students = [
+//     { name: "Tasha", grades: [ 80, 67, 75, 62 ] },
+//     { name: "Tuvok", grades: [ 90, 88, 91, 92 ] },
+//     { name: "Dax", grades: [ 88, 90, 89, 85 ] },
+//     { name: "Neelix", grades: [ 60, 54, 48, 50 ] }
+//   ]
+  
+//   const studentsScholarship = []
+  
+//   for (let i = 0; i < 4; i ++) {
+// 	let gradeTotal = 0 
+// 	for (let j = 0; j < 4; j ++) {
+// 		gradeTotal += students[i].grades[j]
+// 	  }
+// 	  if (gradeTotal / 4 > 80) {
+// 		studentsScholarship.push(students[i])
+// 	  }
+//   }
+  
+//   console.log(studentsScholarship)
+// }
+// selectStudents()
+
+
+// function test(named) {
+//     console.log('Hello ' + named);
+// }
+
+// test('World');
+
+
+// const greet = () => {
+//     return ("hello world");
+// };
