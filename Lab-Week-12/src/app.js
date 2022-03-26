b464804e94a05d2437c5f4e94a46921bb6209bb6
AndrewTake <andrew6041@gmail.com>
2022-03-25 20:41:33 -0700

    part 3, any student in the students Array with an average grade of more than 80 is added to the studentsScholarship Array as an Object in the form { name: gradeAvg: }

--- Lab-Week-12/src/app.js
+++ Lab-Week-12/src/app.js
@@ -15,29 +15,44 @@ darkTheme()
 
 
 
-// function selectStudents() {
-// const students = [
-//     { name: "Tasha", grades: [ 80, 67, 75, 62 ] },
-//     { name: "Tuvok", grades: [ 90, 88, 91, 92 ] },
-//     { name: "Dax", grades: [ 88, 90, 89, 85 ] },
-//     { name: "Neelix", grades: [ 60, 54, 48, 50 ] }
-//   ]
+
+
+const students = [
+    { name: "Tasha", grades: [ 80, 67, 75, 62 ] },
+    { name: "Tuvok", grades: [ 90, 88, 91, 92 ] },
+    { name: "Dax", grades: [ 88, 90, 89, 85 ] },
+    { name: "Neelix", grades: [ 60, 54, 48, 50 ] }
+  ]
+
+
+const StudentGradeAverage = (students) => {
+  const studentsScholarship = []
   
-//   const studentsScholarship = []
-  
-//   for (let i = 0; i < 4; i ++) {
-// 	let gradeTotal = 0 
-// 	for (let j = 0; j < 4; j ++) {
-// 		gradeTotal += students[i].grades[j]
-// 	  }
-// 	  if (gradeTotal / 4 > 80) {
-// 		studentsScholarship.push(students[i])
-// 	  }
-//   }
-  
-//   console.log(studentsScholarship)
-// }
-// selectStudents()
+  for (let i = 0; i < 4; i ++) {
+            // first loop goes through each of the students
+	let gradeTotal = 0 
+	
+    for (let j = 0; j < 4; j ++) {
+            // ++ increments loop
+		gradeTotal += students[i].grades[j]
+            // i = 0 = Tasha, iterates through her grades, then rest of students grades
+	  }
+	  if (gradeTotal / 4 > 80) {
+        let gradeAveStudent = {
+            name: students[i].name,
+            gradeAvg: gradeTotal/4
+        }
+		studentsScholarship.push(gradeAveStudent)
+	  }
+  }
+  console.log(studentsScholarship)
+}
+StudentGradeAverage(students)
+
+
+
+
+
 
 
 // function test(named) {
